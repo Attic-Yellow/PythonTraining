b045bc77f57338be781bbfe5c705fe05354e7fac
loftdoggy <117146643+loftdoggy@users.noreply.github.com>
2023-04-06 13:45:43 +0900

    Add files via upload

---
+++ udemy/day3-1.py
@@ -0,0 +1,8 @@
+## This file is a number check from Odd and Evne ##
+
+number = int(input("Which number do you to check: "))
+
+if (number % 2 == 0):
+  print(number, 'is even number')
+else:
+  print(number, 'is odd number')
